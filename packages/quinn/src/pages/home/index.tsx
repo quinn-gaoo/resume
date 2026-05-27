a3c39ffb8a944580d8809e2c23ff06b90bfa4bc4
quinn-gaoo <quinnn.gao@gmail.com>
2026-05-27 16:58:17 +0800

    update

--- packages/quinn/src/pages/home/index.tsx
+++ packages/quinn/src/pages/home/index.tsx
@@ -69,7 +69,7 @@ function App() {
                   target="_blank"
                   className="rounded-md px-3 py-1.5 text-muted-foreground transition-colors hover:bg-accent hover:text-accent-foreground"
                 >
-                  blog
+                  Blog
                 </a>
               </li>
             </ul>
